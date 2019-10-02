58a612e7ddf0279750979f9c471388fddd0f7245
Shevts0vNI <mollpromking@mail.ru>
2019-10-02 14:36:06 +0300

    dop

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -11,6 +11,10 @@ namespace LAB1
         static void Main(string[] args)
         {
             int a = 10, b = 15, c = 5;
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+            string sharp = "Welcome to C# world, ";
+            Console.WriteLine(sharp + name);
             Console.WriteLine((a + b) / c);
         }
     }
